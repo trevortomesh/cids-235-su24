f7b36f9e4990588a15df5168fc52a594bd1d2f81
trevortomesh <tmtomesh@hotmail.com>
2024-05-24 10:58:01 -0500

    Wrote the Cat class

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    /** The radius of the Circle **/
+    double radius = 1;
+
+    Circle(){
+    }
+
+    Circle(double newRadius){
+        radius = newRadius;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,47 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Scanner input = new Scanner(System.in);
+        //int[] numbers = new int[10];
         Cat genki = new Cat();
-        Cat glitch = new Cat();
+        Cat haku = new Cat();
 
         genki.name = "Genki";
         genki.age = 1;
         genki.color = "orange";
+        genki.weight = 13.0;
 
-        System.out.println(genki.name);
+        haku.name = "Haku";
+        haku.age = 4;
+        haku.color = "Tuxedo";
+        haku.fixed = true;
+        haku.weight = 10.5;
+
+        //System.out.println(genki.name);
+   /* PROCEDURAL WAY
+
         int age = 1;
         boolean fixed = true;
         String color = "orange";
         String name = "Genki";
         double weight = 15.5;
 
-        String name2 = "Glitch";
-        int age2 = 13;
+        String name2 = "Haku";
+        int age2 = 4;
         boolean fixed2 = true;
         String color2 = "Black and White";
-        double weight2 = 13.0;
+        double weight2 = 10.5;
+
+     */
 
         System.out.println("My cat's name is " + genki.name +
                 " he is " + genki.age + " years old " +
-                "and he is " + genki.color + "!");
+                "and he is " + genki.color + "!" +
+                " He weighs " + genki.weight + " lbs");
+
+        Circle circle = new Circle(2.0);
+        System.out.println(circle.radius);
 
     }
 }

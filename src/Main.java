8de14808241b8995fa0f81f242428c75bc864122
trevortomesh <tmtomesh@hotmail.com>
2024-06-28 10:54:26 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,121 +1,57 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
+        Circle[] circleArray;
+        circleArray = createCircleArray();
+        printCircleArray(circleArray);
+    }
 
+    public static Circle[] createCircleArray(){
+        Random rand = new Random();
+        Circle[] circleArray = new Circle[10];  //define an array of 10 circles
 
-        //Scanner input = new Scanner(System.in);
-        //int[] numbers = new int[10];
-        /*
-        Cat genki = new Cat();
-        Cat haku = new Cat();
+        for(int i = 0; i < circleArray.length; i++){
 
-        genki.name = "Genki";
-        genki.age = 1;
-        genki.color = "orange";
-        genki.weight = 13.0;
+            circleArray[i] = new Circle(rand.nextInt(10)+1);
+            //System.out.println("Circle created with radius of " + circleArray[i].getRadius());
+        }
+        return circleArray;
+    }
 
-        haku.name = "Haku";
-        haku.age = 4;
-        haku.color = "Tuxedo";
-        haku.fixed = true;
-        haku.weight = 10.5;
-        */
-        //System.out.println(genki.name);
-        /* PROCEDURAL WAY
+    public static void printCircleArray(Circle[] circleArray){
+        System.out.printf("%-30s%-15s\n", "Radius", "Area");
+        for(int i = 0; i < circleArray.length; i++){
+            System.out.printf("%-30f%-15f\n", circleArray[i].getRadius(),
+                    circleArray[i].getArea());
+        }
 
-        int age = 1;
-        boolean fixed = true;
-        String color = "orange";
-        String name = "Genki";
-        double weight = 15.5;
+        System.out.println("-----------------------------------");
+        System.out.println("The total area of the circles is: " + sum(circleArray));
+    }
 
-        String name2 = "Haku";
-        int age2 = 4;
-        boolean fixed2 = true;
-        String color2 = "Black and White";
-        double weight2 = 10.5;
+    public static double sum(Circle[] circleArray){
+        double sum = 0;
 
-     */
-        /*
-        System.out.println("My cat's name is " + genki.name +
-                " he is " + genki.age + " years old " +
-                "and he is " + genki.color + "!" +
-                " He weighs " + genki.weight + " lbs");
-
-         */
-        /*
-        Circle circle = new Circle(2.0);
-        System.out.println(circle.radius);
-        circle.setRadius(13);
-        System.out.println("circumference of circle is: " + circle.getPerimeter());
-        System.out.println("The area of circle is: " + circle.getArea());
-
-        Circle circle2 = new Circle();
-        System.out.println(circle2.radius);
-
-        Circle c = new Circle(1.2);
-        System.out.println("Value of c is: " + c);
-
-        Circle d = c;
-        System.out.println("Value of d is: " + d);
-
-        d.radius = 1.0;
-        System.out.println("The radius of c is now: " + c.radius);
-
-        Dog mocha = new Dog();
-
-         */
-
-        /*
-        Cat genki = new Cat();
-        Cat haku = new Cat();
-        System.out.println("genki's address is: " + genki);
-        System.out.println("haku's address is: " + haku);
-
-        System.out.println("setting haku = genki");
-        haku = genki;
-        System.out.println("haku's address is now: " + haku);
-        System.out.println("genki's address is now: " + genki);
-        haku.name = "Haku";
-        System.out.println("Haku's name is now " + haku.name);
-        System.out.println("Genki's name is now " + genki.name);
-*/
-
-        Circle circle1 = new Circle();  // creating a circle object with default radius (1)
-        Circle circle2 = new Circle(5); // creating a circle object with radius of 5
-        //Circle.numberOfObjects = 12;
-
-        //System.out.println("The perimeter of the circle is: " + circle1.getPerimeter());
-
-        circle1.setRadius(12);
-        //System.out.println("The radius of the circle1 is now: " + circle1.getRadius());
-        //System.out.println("There are " + Circle.getNumberOfObjects() + " circles!");
-        printCircle(circle1);
-        printCircle(circle2);
-
-
-       // System.out.println(Circle.getNumberOfObjects());
-        //System.out.println(circle1.getNumberOfObjects());
-
-        int a = 5;
-        System.out.println("in main: a in main method is " + a);
-        System.out.println("in main: calling printInt...");
-        printInt(a,a);
-        System.out.println("in main: a is now " + a);
+        for(int i = 0; i < circleArray.length; i++){
+            sum += circleArray[i].getArea();
+        }
+        return sum;
 
     }
 
-    public static void printInt(int b, int a){
-        System.out.println("in method: you passed the value " + b);
-        System.out.println("in method: changing value of b to 0");
-        b = 0;
-        System.out.println("in method: b is now " + b);
-        a = b;
+    public static void printInt(int a){
+        System.out.println("in method: you passed the value " + a);
+        System.out.println("in method: changing value of a to 0");
+        a = 0;
+        System.out.println("in method: a is now " + a);
+
     }
 
     public static void printCircle(Circle c){
         System.out.println("The area of the circle of radius " +
                 c.getRadius() + " is " + c.getArea());
+                //c.setRadius(100);
     }
 }
 

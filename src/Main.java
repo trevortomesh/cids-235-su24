b545a321071ed86c573e29b9edd2ce2e13b6c6d9
trevortomesh <tmtomesh@hotmail.com>
2024-08-05 10:56:27 -0500

    Merge remote-tracking branch 'origin/master'

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,78 @@
 import org.w3c.dom.css.Rect;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<String> cityList = new ArrayList<>();
+
+        cityList.add("Cameron");
+        cityList.add("Mendota Heights");
+        cityList.add("New Richmond");
+        cityList.add("Sarona");
+        cityList.add("River Falls");
+
+        System.out.println(cityList.size());
+        System.out.println(cityList.indexOf("Sarona"));
+        cityList.add("Roberts");
+        System.out.println(cityList.size());
+        System.out.println(cityList);
+
+        /*
         //int a = 5;
         //System.out.println("a is " + a);
         //Integer a = new Integer("5");
-        Integer a = Integer.valueOf("5");
+        Integer a = Integer.valueOf("5"); // set a's value to 5
         System.out.println(a);
 
         //Double b = new Double("2.4");
-        Double b = Double.valueOf("2.4");
+        Double b = a.doubleValue(); // set b's value to 2.4
         System.out.println(b);
 
+        Double c = Double.valueOf("3.5");
+        Integer d = c.intValue();
+        System.out.println(d);
+
+        //Integer x1 = new Integer("32");
+        //Integer x2 = new Integer("32");
+        Integer x1 = Integer.valueOf("32");
+        Integer x2 = Integer.valueOf("32");
+        Integer x3 = 32;
+
+        System.out.println(Long.MAX_VALUE);
+        //Long z = 9223372036854;
+        BigInteger z = new BigInteger("922337203685477580789");
+        System.out.println(z);
+
+        BigDecimal x = new BigDecimal("1.123456789012345678901234567890");
+       // double x = 1.123456789123456783453453453345345345345345345345345345345334523453453453454534534591234;
+        System.out.println(x);
+
+        /*
+        System.out.println(z);
+        System.out.println("x1 is " + x1);
+        System.out.println("x2 is " + x2);
+        System.out.println("x3 is " + x3);
+
+        System.out.println(x1 == x2);
+        System.out.println(x2 == x3);
+         */
+
+        /*
+        System.out.println(c.toString());
 
 
-        /*Cat haku = new Cat("Haku");
+
+        Cat haku = new Cat("Haku");
+        System.out.println(haku.toString());
+        Circle c1 = new Circle();
+        System.out.println(c1); //equivalent to System.out.println(c1.toString());
+        */
+        /*
         haku.speak();
 
         Axolotl wooper = new Axolotl("Wooper");
